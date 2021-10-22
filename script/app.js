1a5f29f98317622c2667114b6fed5ccbe5548ece
Anton Kameniev <anton.kameniev@chisw.com>
2021-10-22 17:56:03 +0300

    created navigation and hero section, added hamburger and media import

--- script/app.js
+++ script/app.js
@@ -1,2 +1,10 @@
 'use strict'
 
+const hamburgerBtn = document.querySelector('.hamburger_container');
+
+const hamburgerSwitch = () => {
+    hamburgerBtn.classList.toggle('hamburger_container-active');
+    
+}
+
+hamburgerBtn.addEventListener('click', hamburgerSwitch)
