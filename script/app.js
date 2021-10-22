643aacf53991c1af326be2c226049ac37917081d
unknown <anton.kamenev69@gmail.com>
2021-10-22 23:29:28 +0300

    update burger menu, create setvice section (one more time)

--- script/app.js
+++ script/app.js
@@ -1,10 +1,11 @@
 'use strict'
 
 const hamburgerBtn = document.querySelector('.hamburger_container');
+const navList = document.querySelector('.header_navigation-list');
 
 const hamburgerSwitch = () => {
     hamburgerBtn.classList.toggle('hamburger_container-active');
-    
+    navList.classList.toggle('header_navigation-list__active')
 }
 
 hamburgerBtn.addEventListener('click', hamburgerSwitch)
